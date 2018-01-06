0f8279b3c147f15ebefff36faa889c99a258347a
nthzs <35150037+nthzs@users.noreply.github.com>
2018-01-06 14:19:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + '  Have a nice day ' + str(type(name))
+	new_name = name + '  Have a nice day'
 	return render_template('index.html', name=new_name)
 
 
